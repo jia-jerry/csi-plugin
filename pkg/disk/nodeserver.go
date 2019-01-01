a8b884c9b1690bf6f9c5d6f4f2fe1c1392a99174
Jerry Jia <jerry.jia@sap.com>
2019-01-01 22:31:08 +0800

    debug info

--- pkg/disk/nodeserver.go
+++ pkg/disk/nodeserver.go
@@ -169,12 +169,14 @@ func (ns *nodeServer) getAttachedDevice(diskId string) (string, error) {
 	}
 
 	disk := disks[0]
-	log.Infof("NodeUnpublishVolume: Get detailed infro for disk %s", diskId)
+	log.Infof("NodePublishVolume: Get detailed infro for disk %s: %v", diskId, disk)
 	if disk.Status == ecs.DiskStatusInUse && disk.InstanceId == instanceId {
 		// From the api, disk.Device always in format like xvda
 		device := disk.Device
+		log.Debug("NodePublishVolume: device: %s", device)
 		if strings.HasPrefix(device, "x") {
 			device = strings.TrimPrefix(device, "x")
+			log.Debug("NodePublishVolume: After trim device: %s", device)
 		}
 		return device, nil
 	} else {
